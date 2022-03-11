c3e450d1739686ae9efc51fe92d548c7f5c5fed6
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-03-11 17:10:54 +0530

    Create small counter application using redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
-  return <div className="App"></div>;
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const decrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const addCountByTen = () => {
+    dispatch({ type: "ADD_COUNT_BY_TEN", payload: 10 });
+  };
+  return (
+    <div>
+      <h1>Counter Application</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addCountByTen}>Add count by 10</button>
+    </div>
+  );
 };
 
 export default App;
